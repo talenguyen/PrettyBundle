51e2936e58c0496751ea51042e77506c55ca2995
tale <giangnguyen.tale@gmail.com>
2015-02-22 15:23:49 +0800

    Implement test for inject method and extras

---
+++ prettybundle/src/main/java/com/tale/prettybundle/Injector.java
@@ -0,0 +1,8 @@
+package com.tale.prettybundle;
+
+/**
+ * Created by tale on 2/18/15.
+ */
+public interface Injector<T> {
+    public void inject(T target);
+}
--- prettybundle/src/main/java/com/tale/prettybundle/PrettyBundle.java
+++ prettybundle/src/main/java/com/tale/prettybundle/PrettyBundle.java
@@ -1,4 +1,94 @@
 package com.tale.prettybundle;
 
-public class PrettyBundle {
+import android.util.Log;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+/**
+ * Bundle "injection" utilities. Use this class to simplify binding bundle values.
+ * <p/>
+ * Injecting bundle from your activity is as easy as:
+ * <pre><code>
+ * public class ExampleActivity extends Activity {
+ *   {@literal @}Extra String stringExtra1;
+ *   {@literal @}Extra String stringExtra2;
+ * <p/>
+ *   {@literal @}Override protected void onCreate(Bundle savedInstanceState) {
+ *     super.onCreate(savedInstanceState);
+ *     PrettyBundle.inject(this);
+ *   }
+ * }
+ * </code></pre>
+ */
+public final class PrettyBundle {
+    public static final String SUFFIX = "$$BundleInjector";
+    public static final String ANDROID_PREFIX = "android.";
+    public static final String JAVA_PREFIX = "java.";
+
+    private PrettyBundle() {
+        throw new AssertionError("No instances.");
+    }
+
+    private static final String TAG = "PrettyBundle";
+    private static boolean debug = false;
+
+    static final Map<Class<?>, Injector<Object>> INJECTORS =
+            new LinkedHashMap<Class<?>, Injector<Object>>();
+
+    private static final Injector<Object> NOP_INJECTOR = new Injector<Object>() {
+        @Override public void inject(Object target) {
+
+        }
+    };
+
+    /**
+     * Control whether debug logging is enabled.
+     */
+    public static void setDebug(boolean debug) {
+        PrettyBundle.debug = debug;
+    }
+
+    public static void inject(Object target) {
+        Class<?> targetClass = target.getClass();
+        try {
+            if (debug) Log.d(TAG, "Looking up view injector for " + targetClass.getName());
+            Injector<Object> injector = findInjectorForClass(targetClass);
+            if (injector != null) {
+                injector.inject(target);
+            }
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to inject views for " + target, e);
+        }
+    }
+
+    private static Injector<Object> findInjectorForClass(Class<?> cls)
+            throws IllegalAccessException, InstantiationException {
+        Injector<Object> injector = INJECTORS.get(cls);
+        if (injector != null) {
+            if (debug) Log.d(TAG, "HIT: Cached in injector map.");
+            return injector;
+        }
+        String clsName = cls.getName();
+        if (clsName.startsWith(ANDROID_PREFIX) || clsName.startsWith(JAVA_PREFIX)) {
+            if (debug) Log.d(TAG, "MISS: Reached framework class. Abandoning search.");
+            return NOP_INJECTOR;
+        }
+
+        try {
+            Class<?> injectorClass = Class.forName(clsName + SUFFIX);
+            //noinspection unchecked
+            injector = (Injector<Object>) injectorClass.newInstance();
+            if (debug) Log.d(TAG, "HIT: Class loaded injection class.");
+        } catch (ClassNotFoundException e) {
+            if (debug) Log.d(TAG, "Not found. Trying superclass " + cls.getSuperclass().getName());
+            injector = findInjectorForClass(cls.getSuperclass());
+        }
+        INJECTORS.put(cls, injector);
+        return injector;
+    }
+
 }
